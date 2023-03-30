8fa9a5687f18f1d75beb71c39cda2acca880ba70
Max Ewaschuk <ewaschukmps@gmail.com>
2023-03-30 11:14:44 -0500

    Episode 4: Images

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,9 +1,12 @@
 package main;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import inputs.KeyboardInputs;
@@ -17,20 +20,37 @@ public class GamePanel extends JPanel{
 	
 	MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
-	private float xDir = 0.003f, yDir = 0.003f;
-	private Color color = new Color(150, 20, 90);
-	private Random random;
+	
+	private BufferedImage img, subImg;
 	
 	public GamePanel() {
-		random = new Random();
 		mouseInputs = new MouseInputs(this);
 		KeyboardInputs keyboardInputs = new KeyboardInputs(this);
+		
+		importImg();
+		
+		setPanelSize();
 		
 		addKeyListener(keyboardInputs);
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 	}
 	
+	private void importImg() {
+		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+		
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setPanelSize() {
+		Dimension size = new Dimension(1280, 800);
+		setPreferredSize(size);
+	}
+
 	public void setRectPos(int x, int y) {
 		this.xDelta = x;
 		this.yDelta = y;
@@ -47,31 +67,7 @@ public class GamePanel extends JPanel{
 	// Override paintComponent from JComponent superclass
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		updateRectangle();
-		g.setColor(color);
-		g.fillRect((int) xDelta, (int) yDelta, 200, 50);
-	}
-	
-	private void updateRectangle() {
-		xDelta += xDir;
-		if(xDelta > 400 || xDelta < 0) {
-			xDir *= -1;
-			color = getRndColor();
-		}
-		
-		yDelta += yDir;
-		if(yDelta > 400 || yDelta < 0) {
-			yDir *= -1;
-			color = getRndColor();
-		}
-	}
-	
-	private Color getRndColor() {
-		int r = random.nextInt(0, 255);
-		int g = random.nextInt(0, 255);
-		int b = random.nextInt(0, 255);
-		
-		return new Color(r, g, b);
+		subImg = img.getSubimage(1*64, 8*40, 64, 40);
+		g.drawImage(subImg, (int) xDelta, (int) yDelta, 128, 80, null);
 	}
 }
